2d2e9feeac2a63595493dfe42c64ff37ac1d522b
Mark Wilcock <mark.wilcock@atos.net>
2021-02-11 10:19:49 +0000

    Initial Commit

---
+++ js/Matrix-script.js
@@ -0,0 +1,13 @@
+  <script type="text/javascript">
+    $(document).ready(function () {
+      $("td:contains('Yes')").html('<img src="https://bitbucket.org/mwilcock/mwilcock.bitbucket.org/raw/d331904ad2ae63dbbaaf00e39c2977e47f67d363/icons/Yes.png" alt="Point Aids this Disability type" height="20px" width="20px"/>');
+    });
+
+    $(document).ready(function () {
+      $("td:contains('partial')").html('<img src="https://bitbucket.org/mwilcock/mwilcock.bitbucket.org/raw/d331904ad2ae63dbbaaf00e39c2977e47f67d363/icons/Circle%20-%20Partial.png" alt="Point partially Aids this Disability type" height="20px" width="20px"/>');
+    });
+
+    $(document).ready(function () {
+      $("td:contains('Not app')").html('<img src="https://bitbucket.org/mwilcock/mwilcock.bitbucket.org/raw/d331904ad2ae63dbbaaf00e39c2977e47f67d363/icons/Dash%20-%20NA.png" alt="Point does not directly benefit this Disability type" height="5px" width="20px"/>');
+    });
+  </script>
